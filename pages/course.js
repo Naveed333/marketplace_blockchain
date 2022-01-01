077561c32127bc4b9dac392f3f4e2aeba64f05cb
Naveed <naveed@naveeds-mbp.home>
2022-01-01 12:10:40 +0500

    base layout 1-Jan-2022

--- pages/course.js
+++ pages/course.js
@@ -1,21 +1,18 @@
 import { Modal } from "@components/common";
-import {
-  CourseHero,
-  Curriculum,
-  Keypoints
-} from "@components/course";
+import { CourseHero, Curriculum, Keypoints } from "@components/course";
 import { BaseLayout } from "@components/layout";
 
 export default function Course() {
-
   return (
-    <BaseLayout>
-      <div className="py-4">
+    <>
+      <div className='py-4'>
         <CourseHero />
       </div>
       <Keypoints />
       <Curriculum />
       <Modal />
-    </BaseLayout>
-  )
+    </>
+  );
 }
+
+Course.Layout = BaseLayout;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,20 @@
-
-import { Hero, Breadcrumbs } from "@components/common"
-import { CourseList } from "@components/course"
-import { BaseLayout } from "@components/layout"
-import { OrderCard } from "@components/order"
-import { EthRates, WalletBar } from "@components/web3"
+import { Hero, Breadcrumbs } from "@components/common";
+import { CourseList } from "@components/course";
+import { BaseLayout } from "@components/layout";
+import { OrderCard } from "@components/order";
+import { EthRates, WalletBar } from "@components/web3";
 
 export default function Home() {
   return (
-    <BaseLayout>
+    <>
       <Hero />
       <Breadcrumbs />
       <WalletBar />
       <EthRates />
       <OrderCard />
       <CourseList />
-    </BaseLayout>
-  )
+    </>
+  );
 }
+
+Home.Layout = BaseLayout;
